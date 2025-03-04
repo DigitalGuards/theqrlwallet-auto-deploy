cb96afc9a9fc942da9c734b39f03bb4b49630b9a
Hiroyuki <hiroyukikumazawa.jp@gmail.com>
2025-03-04 11:27:07 +0000

    feat: add nvm for node version

--- deploy-backend-prod.sh
+++ deploy-backend-prod.sh
@@ -7,6 +7,9 @@ fi
 
 cd "$BACKEND_PROD_PATH"
 
+source "$NVM_PATH"
+nvm use 20.17.0
+
 echo "Pulling latest changes..."
 git pull origin main
 
@@ -15,3 +18,5 @@ npm install
 
 echo "Restarting server..."
 pm2 restart "$BACKEND_PROD_PM2_NAME"
+
+echo "Done"
